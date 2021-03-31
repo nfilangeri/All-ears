debdde4a61ce83e21bc7415a83cdfa6cc37cddf3
Nicolas Oederlin <n.oederlin@gmail.com>
2021-03-31 16:55:33 +0200

    ok

--- app/models/newspaper.rb
+++ app/models/newspaper.rb
@@ -4,14 +4,13 @@ class Newspaper < ApplicationRecord
 
   def get_articles
     uri = URI.parse("https://custom-search.p.rapidapi.com/api/search/CustomNewsSearchAPIV2?q=covid&searchEngineId=8957896738330692472&pageNumber=1")
-    Newspaper.new("BBC News","center")
+    @bcc = Newspaper.new("BBC News", "center")
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     @data = http.get(uri.request_uri, {'Content-Type': 'application/json', 'X-RapidAPI-Host': 'custom-search.p.rapidapi.com', 'X-RapidAPI-Key': "69be2150ffmsh222a28b051beacbp113c09jsn002467b93bc6"})
     list_of_articles = JSON.parse(@data.body)["value"]
     list_of_articles.each do |a|
-      Article.new( title: a.title, description: a.description, content: a.body, publication_date: a.datePublished, image: a.image["url"])
-
+      Article.new(title: a.title, description: a.description, content: a.body, publication_date: a.datePublished, image: a.image["url"], newspaper_id: @bbc.id)
       end
   end
 end
