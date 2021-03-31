15496ebbb08be8376322fe454158503a630eaf80
Nicolas Oederlin <n.oederlin@gmail.com>
2021-03-31 20:00:19 +0200

    homepage / footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'articles#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :articles, only: [:index, :show, :create, :destroy] do
     resources :comments, only: [:create, :destroy]
