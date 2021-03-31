c17acf910a220081f2c2c91db491f73aee39f43c
Manon Barnes <manonbarnes@icloud.com>
2021-03-31 18:17:39 +0200

    api bbc working

--- db/migrate/20210331144954_add_image_and_description_to_newspaper.rb
+++ db/migrate/20210331144954_add_image_and_description_to_newspaper.rb
@@ -1,6 +1,6 @@
 class AddImageAndDescriptionToNewspaper < ActiveRecord::Migration[6.1]
   def change
-    add_column :newspapers, :image, :string
-    add_column :newspapers, :description, :string
+    add_column :articles, :image, :string
+    add_column :articles, :description, :string
   end
 end
