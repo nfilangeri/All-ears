d4b22a411918660de200ffe8dfa571662ec4d91c
Manon Barnes <manonbarnes@icloud.com>
2021-03-31 12:40:59 +0200

    show finished until apis

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,5 @@
 class Rating < ApplicationRecord
   belongs_to :article
   belongs_to :user
+  validates :article_id, uniqueness: { scope: :user_id, message: "You've already placed your scale!" }
 end
