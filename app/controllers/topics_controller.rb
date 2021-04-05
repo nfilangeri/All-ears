9b5942b947331acd6420a8a9a0a0ddfbfdb86ec0
nicolasfilangeri <nicolas.filangeri@live.fr>
2021-04-05 12:15:22 +0200

    create/delete

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,21 +3,18 @@ class TopicsController < ApplicationController
   
   def index
     @topics = Topic.all
+    @topic= Topic.new
   end
 
   def show
     @message = Message.new
   end
 
-  def new
-    @topic = Topic.new
-  end
-
   def create
     @topic = Topic.new(params_topic)
     @topic.user = current_user
     if @topic.save
-      redirect_to topics_path(topic)
+      redirect_to topics_path
     end
   end
 
@@ -29,20 +26,20 @@ class TopicsController < ApplicationController
     end
   end
 
-def destroy
-  @topic.destroy
-  redirect_to topics_path
-end
+  def destroy
+    @topic.destroy
+    redirect_to topics_path
+  end
 
 private
 
-def params_topic
-  params.require(:topic).permit(:subject,:content, :user)
-end
+  def params_topic
+    params.require(:topic).permit(:subject,:content, :user)
+  end
 
-def set_topics
-  @topic = Topic.find(params[:id])
-end
+  def set_topics
+    @topic = Topic.find(params[:id])
+  end
 
 end
 
