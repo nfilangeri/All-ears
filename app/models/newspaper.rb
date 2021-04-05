2816d9e796676a8267ca6fdcb4bce540567ad5cf
nicolasfilangeri <nicolas.filangeri@live.fr>
2021-04-05 14:59:28 +0200

    error 500

--- app/models/newspaper.rb
+++ app/models/newspaper.rb
@@ -9,6 +9,8 @@ class Newspaper < ApplicationRecord
     data = http.get(uri.request_uri, {'Content-Type': 'application/json', 'X-RapidAPI-Host': 'custom-search.p.rapidapi.com', 'X-RapidAPI-Key': "69be2150ffmsh222a28b051beacbp113c09jsn002467b93bc6" })
     list_of_articles = JSON.parse(data.body)["value"]
     a = list_of_articles.first
-    article = Article.create(search:search, title: a["title"], description: a["description"], content: a["body"], publication_date: a["datePublished"], image: a["image"]["url"], newspaper_id: self.id)
+    unless a.nil?
+      article = Article.create(search:search, title: a["title"], description: a["description"], content: a["body"], publication_date: a["datePublished"], image: a["image"]["url"], newspaper_id: self.id)
+    end
   end
 end
